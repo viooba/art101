5cf7b2cc65eba1b3ab9d8932651d232aba0b51af
viooba <vilei122117@gmail.com>
2024-06-08 22:33:25 -0700

    a
    a

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,42 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - lab 6
+// Author: Viona Lei
+// Date:6/8/24
 
 // Constants
-
+let myTransport = ["car", "bike", "bus", "train", "walking"];
 // Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+var MyMainRide = {
+  make:"Toyota",
+  model: "Corolla",
+  color: "silver",
+  year: 2019,
+  age: function() {
+      return 2019 - 2024;
+  }
 }
+  
+//output 
+document.writeln("Kinds of transportation I use: ", myTransport, "<b/r>");
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+document.writeln("My Main Ride: <pre>", 
+    JSON.stringify(MyMainRide, null, '/t'), "</pre>");
 
-// let's get this party started
-main();
+
+
+
+
+
+// // this is an example function and this comment tells what it doees and what parameters are passed to it.
+// function myFunction(param1, param2) {
+//   // some code here
+//   // return results;
+// }
+
+// function main() {
+//   console.log("Main function started.");
+//   // the code that makes everything happen
+// }
+
+// // let's get this party started
+// main();
