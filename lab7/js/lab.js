b47839e22145efa8806342df413422e6561c2824
viooba <vilei122117@gmail.com>
2024-06-08 23:01:54 -0700

    Update lab.js

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,22 +1,40 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Functions
+// Author: Viona Lei
+// Date:6/8/24
 
 // Constants
 
 // Functions
 
+function sortUserName() {
+  var userName = window.prompt("Hi. Please tell me your name so I can it.");
+  console.log("userName =", userName); 
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+  var userArray = userName.split('');
+  console.log("userArray =", nameArray);
+
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort= ", nameArraySort);
+
+  var nameSorted = nameArraySorted.join('');
+  console.log("nameSorted =", nameSorted);
+
+  return nameSorted
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+document.writeln("Oh hey, I've fixed your name: ",
+  sortUserName(), "</br>") ; 
 
-// let's get this party started
-main();
+// // this is an example function and this comment tells what it doees and what parameters are passed to it.
+// function myFunction(param1, param2) {
+//   // some code here
+//   // return results;
+// }
+
+// function main() {
+//   console.log("Main function started.");
+//   // the code that makes everything happen
+// }
+
+// // let's get this party started
+// main();
