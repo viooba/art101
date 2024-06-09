2cb14552b182c6d6d8be99285eade2541b3c3626
viooba <vilei122117@gmail.com>
2024-06-08 17:12:52 -0700

    a
    a

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -6,6 +6,7 @@
 
 // Functions
 
+
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
