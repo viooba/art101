9a8e0cc42cee83ea34e08bd5dbd7f83d360c8955
viooba <vilei122117@gmail.com>
2024-06-09 19:10:48 -0700

    Update lab.js

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,22 +1,39 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Viona Lei
+// Date: 6/8/24
 
 // Constants
-
+ 
 // Functions
-
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function isOdd(x) {
+  return (x % 2 != 0);
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+console.log("Is 3 odd?", isOdd(3));
+console.log("Is 4 odd?", isOdd(4));
 
-// let's get this party started
-main();
+
+array = [101, 81, 5, 17, 43, 144, 10000]
+console.log("My Array", array);
+var result = array.map(isOdd);
+console.log("Test of Oddness of array:", result);
+
+var result = array.map(function(x){
+  return x ** 0.5;
+})
+
+console.log("Squareroot of array:", result);
+
+// // this is an example function and this comment tells what it doees and what parameters are passed to it.
+// function myFunction(param1, param2) {
+//   // some code here
+//   // return results;
+// }
+
+// function main() {
+//   console.log("Main function started.");
+//   // the code that makes everything happen
+// }
+
+// // let's get this party started
+// main();
